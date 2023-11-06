47aad4b195da146d4c012878207a4c334f34b48d
rockyleishman <rockyleishman@gmail.com>
2023-11-06 13:19:41 -0700

    Events added
    Game event & attack event scripts added

---
+++ Assets/Scripts/ScriptableObjects/Events/AttackEvent.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AttackEventObject", menuName = "Event/AttackEvent", order = 0)]
+public class AttackEvent : ScriptableObject
+{
+    private List<AttackEventListener> _listeners = new List<AttackEventListener>();
+
+    public void AddListener(AttackEventListener listener)
+    {
+        _listeners.Add(listener);
+    }
+
+    public void RemoveListener(AttackEventListener listener)
+    {
+        _listeners.Remove(listener);
+    }
+
+    public void TriggerEvent(Vector3 attackOrigin, Vector3 attackDirection, MonoBehaviour attackTarget)
+    {
+        foreach (AttackEventListener listener in _listeners)
+        {
+            listener.OnTriggered(attackOrigin, attackDirection, attackTarget);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/GameEvent.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEventObject", menuName = "Event/GameEvent", order = 0)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> _listeners = new List<GameEventListener>();
+
+    public void AddListener(GameEventListener listener)
+    {
+        _listeners.Add(listener);
+    }
+
+    public void RemoveListener(GameEventListener listener)
+    {
+        _listeners.Remove(listener);
+    }
+
+    public void TriggerEvent(Vector3 position)
+    {
+        foreach (GameEventListener listener in _listeners)
+        {
+            listener.OnTriggered(position);
+        }
+    }
+}
