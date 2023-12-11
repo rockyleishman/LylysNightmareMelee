2882029d7f11968bbb34876094d4218c230e8894
cochuthonho <128014810+cochuthonho@users.noreply.github.com>
2023-12-10 20:16:17 -0700

    Auto stash before merge of "main" and "origin/main"
    Update new art

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
